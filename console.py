c92bbb7856c13eccd64b43ce16cd021fcd4eef22
Owen Gitau <owengitau02@gmail.com>
2024-01-11 01:28:49 -0800

    console

--- console.py
+++ console.py
@@ -27,6 +27,76 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Overides the emptyline method"""
         return False
+    
+    def do_create(self, args):
+        """creates classes objects"""
+        if not args:
+            print("** class name missing **")
+            return
+        elif args not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = HBNBCommand.classes[args]()
+        storage.save()
+        print(new_instance.id)
+        storage.save()
+   
+    def do_show(self, args):
+       """method to show objects"""
+       n = args.partition(" ")
+       ob_name = n[0]
+       ob_id = n[2]
+
+       if ob_id and ' ' in ob_id:
+           ob_id = ob_id.partition(' ')[0]
+
+       if not ob_name:
+           print("** class name missing **")
+           return
+        
+       if ob_name not in HBNBCommand.classes:
+            print("** class doen't exist **")
+            return
+
+       if not ob_id:
+            print("** instance id missing **")
+            return
+
+       key = ob_name + "." + ob_id
+
+       try:
+           print(storage._FileStorage__objects[key])
+       except KeyError:
+           print("** no instance found **")
+    
+    def do_destroy(self, args):
+        """Destroys indicated object"""
+        n = args.partition(" ")
+        ob_name = n[0]
+        ob_id = n[2]
+
+        if ob_id and ' ' in ob_id:
+            ob_id = ob_id.partition(' ')[0]
+
+        if not ob_name:
+            print("** class name missing **")
+            return
+
+        if ob_name not in HBNBCommand.classes:
+            print("** class doen't exist **")
+            return
+
+        if not ob_id:
+            print("** instance id missing **")
+            return
+
+        key = ob_name + "." + ob_id
+
+        try:
+            del(storage.all()[key])
+            storage.save()
+        except KeyError:
+            print("** no instance found **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
