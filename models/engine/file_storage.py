d50116d71d805b528e13be3810a6030e62b39e2d
Owen Gitau <owengitau02@gmail.com>
2024-01-13 06:23:45 -0800

    changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,34 +39,32 @@ class FileStorage:
             x_objs[key] = value.to_dict()
 
         with open(self.__file_path, 'w', encoding='utf-8') as y:
-            x_objs = OrderedDict()
-            for key, value in self.__objects.items():
-                x_objs[key] = value.to_dict()
-
             json.dump(x_objs, y, ensure_ascii=False, indent=4)
 
     def reload(self):
-        """
-        deserializes the JSON file to __objects (only if the JSON file
-        """
-        from models.base_model import BaseModel
-        if isfile(self.__file_path):
-            with open(self.__file_path, 'r', encoding='utf-8') as y:
-                n = y.read()
-                if n:
+        """Deserializes the JSON file to __objects (only if the JSON file exists)"""
+    if isfile(self.__file_path):
+        with open(self.__file_path, 'r', encoding='utf-8') as y:
+            n = y.read()
+            if n:
+                try:
                     store = json.loads(n)
                     for key, t in store.items():
                         name, obj_id = key.split('.')
                         if name in self.classes:
                             cls_type = self.classes[name]
                             obj_instance = cls_type(**t)
-                            if 'created_at' in value:
-                                obj.created_at = datetime.strptime(value['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                                if 'updated_at' in value:
-                                    obj.updated_at = datetime.strptime(value['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                                    self.__objects[key] = obj_instance
+                            if 'created_at' in t:
+                                obj_instance.created_at = datetime.strptime(t['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                            if 'updated_at' in t:
+                                obj_instance.updated_at = datetime.strptime(t['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                            self.__objects[key] = obj_instance
 
-                    return self.__objects
+                except json.JSONDecodeError as e:
+                    print(f"Error decoding JSON: {e}")
+
+    return self.__objects
+
     @property
     def classes(self):
         """
